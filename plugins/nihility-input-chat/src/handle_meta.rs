87f249ac27ac55c9be3d0b1692f35441de64dba5
nihilityer <nihilityer@mail.nihilityer.top>
2025-03-21 07:50:06 +0800

    feat(chat): chat插件正常心跳忽略

--- plugins/nihility-input-chat/src/handle_meta.rs
+++ plugins/nihility-input-chat/src/handle_meta.rs
@@ -1,13 +1,23 @@
 use onebot_v11::event::meta::Meta;
-use tracing::info;
+use serde_json::Value;
+use tracing::{error, info, warn};
 
 pub(super) async fn handle_meta(meta: Meta) {
     match meta {
         Meta::Lifecycle(lifecycle) => {
             info!("Lifecycle event: {:?}", lifecycle);
         }
-        Meta::Heartbeat(heartbeat) => {
-            info!("Heartbeat event: {:?}", heartbeat);
+        Meta::Heartbeat(heartbeat) => match heartbeat.status {
+            Value::Object(object) => {
+                if let Some(good) = object.get("good") {
+                    if *good != Value::Bool(true) {
+                        error!("Heartbeat Exception");
+                    }
+                }
+            }
+            other => {
+                warn!("Other Heartbeat Status: {:?}", other);
+            }
         }
     }
 }
